615be7ec1b31f5f15abee012ac178f815fd92a8e
Yuriy Bondarenko <bondarenko-yri@yandex.ru>
2021-09-08 19:36:48 +0300

    1122334455

--- src/components/text_field/text_field.js
+++ src/components/text_field/text_field.js
@@ -1,6 +1,12 @@
 import './text_field.scss';
 
-const input_text_field = document.getElementById('input_text_field');
-input_text_field.onfocus = function() { form_text_field.classList.add('form_text_field-focus'); };
+// const inputTextField = document.getElementById('input_text_field');
+// inputTextField.onfocus = () => form_text_field.classList.add('form_text_field-focus');
 
-input_text_field.onblur = function() { form_text_field.classList.remove('form_text_field-focus'); };
+// inputTextField.onblur = () => form_text_field.classList.remove('form_text_field-focus');
+
+const input = document.getElementById('input');
+
+input.onfocus = () => form.classList.add('focus');
+
+input.onblur = () => form.classList.remove('focus');
