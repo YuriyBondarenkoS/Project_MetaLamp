5d86711b954621361f460f52b41c16e1232647d8
Yuriy Bondarenko <bondarenko-yri@yandex.ru>
2021-09-09 00:49:18 +0300

    priblem_js_text_field

--- src/components/text_field/text_field.js
+++ src/components/text_field/text_field.js
@@ -1,14 +1,7 @@
 import './text_field.scss';
 
-const input_text_field = document.getElementById('input_text_field');
-const form_text_field = document.getElementById('form_text_field');
+const input_text_field = document.querySelector('.input_text_field');
+const form_text_field = document.querySelector('.form_text_field');
+
 input_text_field.onfocus = () => form_text_field.classList.add('focus_text_field');
 input_text_field.onblur = () => form_text_field.classList.remove('focus_text_field');
-
-// inputTextField.onblur = () => form_text_field.classList.remove('form_text_field-focus');
-
-const input = document.getElementById('input');
-
-input.onfocus = () => form.classList.add('focus');
-
-input.onblur = () => form.classList.remove('focus');
