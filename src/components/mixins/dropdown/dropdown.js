b34124217d55fb1781172fb339c5c529b93d6552
Yuriy Bondarenko <bondarenko-yri@yandex.ru>
2021-09-10 23:56:37 +0300

    dropdown_start_js

--- src/components/mixins/dropdown/dropdown.js
+++ src/components/mixins/dropdown/dropdown.js
@@ -1,2 +1,7 @@
 import './dropdown.scss';
 
+const clickGuests = document.querySelector('.dropdown-toggle');
+
+clickGuests.addEventListener('click', (e) => {
+    e.target.classList.add('.is-show');
+});
