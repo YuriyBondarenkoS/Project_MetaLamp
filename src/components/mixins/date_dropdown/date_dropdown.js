ec9f3b7ca6c0070494f1e3b51385cd8e6596cbc3
Yuriy Bondarenko <bondarenko-yri@yandex.ru>
2021-09-16 00:28:55 +0300

    calendar_style

--- src/components/mixins/date_dropdown/date_dropdown.js
+++ src/components/mixins/date_dropdown/date_dropdown.js
@@ -4,24 +4,28 @@ import '../../../../node_modules/air-datepicker/dist/css/datepicker.css';
 import 'air-datepicker/dist/js/datepicker';
 
 
-let myDatepicker = $('#date_dropdown-datepicker').datepicker().data('datepicker');
+let myDatepickerPiev = $('#date_dropdown-datepicker').datepicker().data('datepicker');
+let myDatepickerNext = $('#date_dropdown-datepicker-next').datepicker().data('datepicker');
 
 $('#date_dropdown-datepicker').datepicker({
     todayButton: true,
     clearButton: true,
+    range: true,
+    onSelect: function (fd, d, picker) { 
+        $("#date_dropdown-datepicker").val(fd.split("-")[0]);
+        $("#date_dropdown-datepicker-next").val(fd.split("-")[1]);
+    }
 });
+
 $('.datepicker--button[data-action="today"]').on('click', () => {
-    myDatepicker.hide();
+    myDatepickerPiev.hide();
 });
 
-// $('#my-datepicker').datepicker({clearButton: true});
-// $( function() {
-//     $("#my-datepicker").datepicker(
-//         {inline: true}
-//     );
-//   } );
-// $("#my-datepicker").datepicker({
-//    inline: true
-// });
+$('#date_dropdown-datepicker-next').on('click', () => {
+    myDatepickerNext.destroy();
+    myDatepickerPiev.show();
+});
 
-// console.log($( "#my-dsatepicker" ));
+$('.date_dropdown__btn').on('click', () => {
+    myDatepickerPiev.show();
+});
