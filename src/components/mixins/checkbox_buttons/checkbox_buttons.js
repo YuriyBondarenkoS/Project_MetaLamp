4a4f89bcd329c9a02e113580fd44b04d9346081d
Yuriy Bondarenko <bondarenko-yri@yandex.ru>
2021-09-19 23:34:48 +0300

    checkbox_buttons_js

--- src/components/mixins/checkbox_buttons/checkbox_buttons.js
+++ src/components/mixins/checkbox_buttons/checkbox_buttons.js
@@ -1,9 +1,27 @@
 import './checkbox_buttons.scss';
 
+const checkboxBtnRectangleSmoke = document.querySelector('#checkbox_buttons__rectangle_smoke'),
+    checkboxBtnRectanglePets = document.querySelector('#checkbox_buttons__rectangle_pets'),
+    checkboxBtnRectangleGuests = document.querySelector('#checkbox_buttons__rectangle_guests'),
+    checkboxBtnSvgSmoke = document.querySelector('#checkbox_buttons__svg_smoke'),
+    checkboxBtnSvgPets = document.querySelector('#checkbox_buttons__svg_pets'),
+    checkboxBtnSvgGuests = document.querySelector('#checkbox_buttons__svg_guests');
 
-const checkboxButtonsRectangle = document.querySelector('.checkbox_buttons__rectangle'),
-    checkboxButtonsSvg = document.querySelector('.checkbox_buttons__svg');
-      
-checkboxButtonsRectangle.addEventListener('click', () => {
-    
-})
+function checkboxButtons(btnCheckbox, svgCheckbox) {
+    btnCheckbox.classList.toggle('checkbox_buttons__rectangle-off');
+    btnCheckbox.classList.toggle('checkbox_buttons__rectangle-on');
+    // svgCheckbox.classList.toggle('checkbox_buttons__svg-off');
+    // svgCheckbox.classList.toggle('checkbox_buttons__svg-on');
+}
+
+checkboxBtnRectangleSmoke.addEventListener('click', () => {
+    checkboxButtons(checkboxBtnRectangleSmoke, checkboxBtnSvgSmoke);
+});
+
+checkboxBtnRectanglePets.addEventListener('click', () => {
+    checkboxButtons(checkboxBtnRectanglePets, checkboxBtnSvgPets);
+});
+
+checkboxBtnRectangleGuests.addEventListener('click', () => {
+    checkboxButtons(checkboxBtnRectangleGuests, checkboxBtnSvgGuests);
+});
