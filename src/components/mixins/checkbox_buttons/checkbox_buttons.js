d2c36867d9039ffcf618e913166d6f66429f951c
Yuriy Bondarenko <bondarenko-yri@yandex.ru>
2021-09-19 16:21:25 +0300

    checkbox_buttons_end

--- src/components/mixins/checkbox_buttons/checkbox_buttons.js
+++ src/components/mixins/checkbox_buttons/checkbox_buttons.js
@@ -1,1 +1,9 @@
 import './checkbox_buttons.scss';
+
+
+const checkboxButtonsRectangle = document.querySelector('.checkbox_buttons__rectangle'),
+    checkboxButtonsSvg = document.querySelector('.checkbox_buttons__svg');
+      
+checkboxButtonsRectangle.addEventListener('click', () => {
+    
+})
