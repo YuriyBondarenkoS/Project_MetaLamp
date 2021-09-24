970feb62153fc153a8da16bf87235b51ff2270cd
Yuriy Bondarenko <bondarenko-yri@yandex.ru>
2021-09-24 21:01:25 +0300

    rate_button_end

--- src/components/mixins/rate_button/rate_button.js
+++ src/components/mixins/rate_button/rate_button.js
@@ -1,158 +1,150 @@
 import './rate_button.scss';
 
 
-const rateBtn1off = document.querySelector('#rate_btn__1'),
-    rateBtn2off = document.querySelector('#rate_btn__2'),
-    rateBtn3off = document.querySelector('#rate_btn__3'),
-    rateBtn4off = document.querySelector('#rate_btn__4'),
-    rateBtn5off = document.querySelector('#rate_btn__5'),
-    rateBtn1on = document.querySelector('#rate_btn__1-on'),
-    rateBtn2on = document.querySelector('#rate_btn__2-on'),
-    rateBtn3on = document.querySelector('#rate_btn__3-on'),
-    rateBtn4on = document.querySelector('#rate_btn__4-on'),
-    rateBtn5on = document.querySelector('#rate_btn__5-on'),
-    pathStar1 = document.querySelector('.rate_button__item');
+const rateBtn1 = document.getElementById("rate_btn__1"),
+    rateBtn2 = document.querySelector('#rate_btn__2'),
+    rateBtn3 = document.querySelector('#rate_btn__3'),
+    rateBtn4 = document.querySelector('#rate_btn__4'),
+    rateBtn5 = document.querySelector('#rate_btn__5');
 
 let star1ClickCounter = 0,
     star2ClickCounter = 0,
     star3ClickCounter = 0,
     star4ClickCounter = 0,
     star5ClickCounter = 0;
-    
-    
-rateBtn1off.addEventListener('mouseenter', () => {
 
-    pathStar1.innerHTML = `<path id="rate_btn__1" d="M10 15.25L3.8125 19L5.45312 11.9688L0.015625 7.23438L7.1875 6.625L10 0.015625L12.8125 6.625L19.9844 7.23438L14.5469 11.9688L16.1875 19L10 15.25Z" fill="url(#paint0_linear)"></path>`;
-    rateBtn1off.before(pathStar1);
-    rateBtn1off.remove();
-    
-    
-    // star1ClickCounter = 0;
-    // rateBtn1off.classList.add('rate_button__item-off');
-    // rateBtn1on.classList.remove('rate_button__item-off');
-});
+function starOn(idStar) {
+    idStar.setAttribute("d", "M10 15.25L3.8125 19L5.45312 11.9688L0.015625 7.23438L7.1875 6.625L10 0.015625L12.8125 6.625L19.9844 7.23438L14.5469 11.9688L16.1875 19L10 15.25Z");
+}
 
-rateBtn1off.addEventListener('mouseleave', () => {
+function starOff(idStar) {
 
-    pathStar1.innerHTML = `<path id="rate_btn__1" d="M10 13.4219L13.75 15.6719L12.7656 11.4062L16.0938 8.5L11.6875 8.125L10 4.09375L8.3125 8.125L3.90625 8.5L7.23438 11.4062L6.25 15.6719L10 13.4219ZM19.9844 7.23438L14.5469 11.9688L16.1875 19L10 15.25L3.8125 19L5.45312 11.9688L0.015625 7.23438L7.1875 6.625L10 0.015625L12.8125 6.625L19.9844 7.23438Z" fill="url(#paint0_linear)"></path>`;
-    rateBtn1off.before(pathStar1);
-    rateBtn1off.remove();
+    if((idStar === rateBtn1 && star1ClickCounter === 0) || (idStar === rateBtn2 && star2ClickCounter === 0) || (idStar === rateBtn3 && star3ClickCounter === 0) || (idStar === rateBtn4 && star4ClickCounter === 0) || (idStar === rateBtn5 && star5ClickCounter === 0)) {
+        idStar.setAttribute("d", "M10 13.4219L13.75 15.6719L12.7656 11.4062L16.0938 8.5L11.6875 8.125L10 4.09375L8.3125 8.125L3.90625 8.5L7.23438 11.4062L6.25 15.6719L10 13.4219ZM19.9844 7.23438L14.5469 11.9688L16.1875 19L10 15.25L3.8125 19L5.45312 11.9688L0.015625 7.23438L7.1875 6.625L10 0.015625L12.8125 6.625L19.9844 7.23438Z");
+    }
+}
 
-        // if (star1ClickCounter === 0) {
-        //     rateBtn1off.classList.remove('rate_button__item-off');
-        //     rateBtn1on.classList.add('rate_button__item-off');
-        // }  
-    });
-
-// rateBtn1off.addEventListener('click', () => {
-//     star1ClickCounter = 1;
-// });
-
-// rateBtn2off.addEventListener('click', () => {
-//     star2ClickCounter = 1;
-// });
-
-// rateBtn3off.addEventListener('click', () => {
-//     star3ClickCounter = 1;
-// });
-
-// rateBtn4off.addEventListener('click', () => {
-//     star4ClickCounter = 1;
-// });
-
-// rateBtn5off.addEventListener('click', () => {
-//     star5ClickCounter = 1;
-// });
-
-
-rateBtn2off.addEventListener('mouseenter', () => {
+function offClick() {
+    star1ClickCounter = 0;
     star2ClickCounter = 0;
-    rateBtn1off.classList.add('rate_button__item-off');
-    rateBtn1on.classList.remove('rate_button__item-off');
-    rateBtn2off.classList.add('rate_button__item-off');
-    rateBtn2on.classList.remove('rate_button__item-off');
-});
-
-rateBtn2off.addEventListener('mouseleave', () => {
-    if (star2ClickCounter === 0) {
-        rateBtn1off.classList.remove('rate_button__item-off');
-        rateBtn1on.classList.add('rate_button__item-off');
-        rateBtn2off.classList.remove('rate_button__item-off');
-        rateBtn2on.classList.add('rate_button__item-off');
-    }
-});
-
-rateBtn3off.addEventListener('mouseenter', () => {
     star3ClickCounter = 0;
-    rateBtn1off.classList.add('rate_button__item-off');
-    rateBtn1on.classList.remove('rate_button__item-off');
-    rateBtn2off.classList.add('rate_button__item-off');
-    rateBtn2on.classList.remove('rate_button__item-off');
-    rateBtn3off.classList.add('rate_button__item-off');
-    rateBtn3on.classList.remove('rate_button__item-off');
-});
-
-rateBtn3off.addEventListener('mouseleave', () => {
-    if (star3ClickCounter === 0) {
-        rateBtn1off.classList.remove('rate_button__item-off');
-        rateBtn1on.classList.add('rate_button__item-off');
-        rateBtn2off.classList.remove('rate_button__item-off');
-        rateBtn2on.classList.add('rate_button__item-off');
-        rateBtn3off.classList.remove('rate_button__item-off');
-        rateBtn3on.classList.add('rate_button__item-off');
-    }
-});
-
-rateBtn4off.addEventListener('mouseenter', () => {
     star4ClickCounter = 0;
-    rateBtn1off.classList.add('rate_button__item-off');
-    rateBtn1on.classList.remove('rate_button__item-off');
-    rateBtn2off.classList.add('rate_button__item-off');
-    rateBtn2on.classList.remove('rate_button__item-off');
-    rateBtn3off.classList.add('rate_button__item-off');
-    rateBtn3on.classList.remove('rate_button__item-off');
-    rateBtn4off.classList.add('rate_button__item-off');
-    rateBtn4on.classList.remove('rate_button__item-off');
-});
-
-rateBtn4off.addEventListener('mouseleave', () => {
-    if (star4ClickCounter === 0) {
-        rateBtn1off.classList.remove('rate_button__item-off');
-        rateBtn1on.classList.add('rate_button__item-off');
-        rateBtn2off.classList.remove('rate_button__item-off');
-        rateBtn2on.classList.add('rate_button__item-off');
-        rateBtn3off.classList.remove('rate_button__item-off');
-        rateBtn3on.classList.add('rate_button__item-off');
-        rateBtn4off.classList.remove('rate_button__item-off');
-        rateBtn4on.classList.add('rate_button__item-off');
-    }
-});
-
-rateBtn5off.addEventListener('mouseenter', () => {
     star5ClickCounter = 0;
-    rateBtn1off.classList.add('rate_button__item-off');
-    rateBtn1on.classList.remove('rate_button__item-off');
-    rateBtn2off.classList.add('rate_button__item-off');
-    rateBtn2on.classList.remove('rate_button__item-off');
-    rateBtn3off.classList.add('rate_button__item-off');
-    rateBtn3on.classList.remove('rate_button__item-off');
-    rateBtn4off.classList.add('rate_button__item-off');
-    rateBtn4on.classList.remove('rate_button__item-off');
-    rateBtn5off.classList.add('rate_button__item-off');
-    rateBtn5on.classList.remove('rate_button__item-off');
+    starOff(rateBtn1);
+    starOff(rateBtn2);
+    starOff(rateBtn3);
+    starOff(rateBtn4);
+    starOff(rateBtn5);
+}
+
+rateBtn1.addEventListener('click', () => {
+    offClick();
+    star1ClickCounter += 1;
+    starOn(rateBtn1);
 });
 
-rateBtn5off.addEventListener('mouseleave', () => {
-    if (star5ClickCounter === 0) {
-        rateBtn1off.classList.remove('rate_button__item-off');
-        rateBtn1on.classList.add('rate_button__item-off');
-        rateBtn2off.classList.remove('rate_button__item-off');
-        rateBtn2on.classList.add('rate_button__item-off');
-        rateBtn3off.classList.remove('rate_button__item-off');
-        rateBtn3on.classList.add('rate_button__item-off');
-        rateBtn4off.classList.remove('rate_button__item-off');
-        rateBtn4on.classList.add('rate_button__item-off');
-        rateBtn5off.classList.remove('rate_button__item-off');
-        rateBtn5on.classList.add('rate_button__item-off');
+rateBtn2.addEventListener('click', () => {
+    offClick();
+    star1ClickCounter += 1;
+    star2ClickCounter += 1;
+    starOn(rateBtn1);
+    starOn(rateBtn2);
+});
+
+rateBtn3.addEventListener('click', () => {
+    offClick();
+    star1ClickCounter += 1;
+    star2ClickCounter += 1;
+    star3ClickCounter += 1;
+    starOn(rateBtn1);
+    starOn(rateBtn2);
+    starOn(rateBtn3);
+});
+ 
+rateBtn4.addEventListener('click', () => {
+    offClick();
+    star1ClickCounter += 1;
+    star2ClickCounter += 1;
+    star3ClickCounter += 1;
+    star4ClickCounter += 1;
+    starOn(rateBtn1);
+    starOn(rateBtn2);
+    starOn(rateBtn3);
+    starOn(rateBtn4);
+});
+
+rateBtn5.addEventListener('click', () => {
+    offClick();
+    star1ClickCounter += 1;
+    star2ClickCounter += 1;
+    star3ClickCounter += 1;
+    star4ClickCounter += 1;
+    star5ClickCounter += 1;
+    starOn(rateBtn1);
+    starOn(rateBtn2);
+    starOn(rateBtn3);
+    starOn(rateBtn4);
+    starOn(rateBtn5);
+});
+ 
+rateBtn1.addEventListener('mouseenter', () => {
+    starOn(rateBtn1);
+});
+
+rateBtn2.addEventListener('mouseenter', () => {
+    starOn(rateBtn1);
+    starOn(rateBtn2);
+});
+
+rateBtn3.addEventListener('mouseenter', () => {
+    starOn(rateBtn1);
+    starOn(rateBtn2);
+    starOn(rateBtn3);
+});
+
+rateBtn4.addEventListener('mouseenter', () => {
+    starOn(rateBtn1);
+    starOn(rateBtn2);
+    starOn(rateBtn3);
+    starOn(rateBtn4);
+});
+
+rateBtn5.addEventListener('mouseenter', () => {
+    starOn(rateBtn1);
+    starOn(rateBtn2);
+    starOn(rateBtn3);
+    starOn(rateBtn4);
+    starOn(rateBtn5);
+});
+
+rateBtn1.addEventListener('mouseleave', () => {
+    if (star1ClickCounter === 0) {
+        starOff(rateBtn1); 
     }
+});
+
+rateBtn2.addEventListener('mouseleave', () => {
+    starOff(rateBtn1);
+    starOff(rateBtn2);
+});
+
+rateBtn3.addEventListener('mouseleave', () => {
+    starOff(rateBtn1);
+    starOff(rateBtn2);
+    starOff(rateBtn3);
+});
+
+rateBtn4.addEventListener('mouseleave', () => {
+    starOff(rateBtn1);
+    starOff(rateBtn2);
+    starOff(rateBtn3);
+    starOff(rateBtn4);
+});
+
+rateBtn5.addEventListener('mouseleave', () => {
+    starOff(rateBtn1);
+    starOff(rateBtn2);
+    starOff(rateBtn3);
+    starOff(rateBtn4);
+    starOff(rateBtn5);
 });
