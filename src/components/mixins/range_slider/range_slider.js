2fcbd538780e77bc43e9e12d6bda0da0a40788dd
Yuriy Bondarenko <bondarenko-yri@yandex.ru>
2021-09-27 23:18:48 +0300

    range_slider_end

--- src/components/mixins/range_slider/range_slider.js
+++ src/components/mixins/range_slider/range_slider.js
@@ -3,56 +3,16 @@ import './range_slider.scss';
 import '../../../../node_modules/ion-rangeslider/css/ion.rangeSlider.css';
 import 'ion-rangeslider/js/ion.rangeSlider';
 
-// Launch plugin
-// $("#range").ionRangeSlider({
-//     type: "double",
-//     min: 0,
-//     max: 1000,
-//     from: 200,
-//     to: 500
-// });
-
-// // Saving it's instance to var
-// var slider = $("#range").data("ionRangeSlider");
-
-// // Get values
-// var from = slider.result.from;
-// var to = slider.result.to;
-
 $(".js-range-slider").ionRangeSlider({
-    
-    // console.log(onChange);
     onChange: function (data) {
-        console.log(onChange);
-        console.log(data.from);
+        rangePrice(data.from, data.to);
     },
-
     onFinish: function (data) {
-        console.log(data.to);
+        rangePrice(data.from, data.to);
     },
-
 });
 
-var slider = $(".js-range-slider").data("ionRangeSlider");
-
-console.log($(".js-range-slider").data());
-console.log(slider);
-
-var low = $(".js-range-slider").data().from;
-var high = $(".js-range-slider").data().to;
-
-console.log($(".js-range-slider").data());
-
-console.log(low);
-console.log(high);
-
-// let slider = $("#range").data("ionRangeSlider");
-    
-// $("#range_slider__content").find(function (e) {
-        		
-//     let from = slider.range_slider__content.from,
-//         to = slider.range_slider__content.to;
-
-//     let resultString = "Result: from: " + from + " to: " + to;
-//     $("#range_slider__content").html(resultString);
-// });
+function rangePrice(from, to) {
+    let resultString = `${from}₽ - ${to}₽`;
+    $("#range_slider__content").html(resultString);
+}
