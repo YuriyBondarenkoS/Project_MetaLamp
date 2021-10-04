7a274d0334555f1d99a029ef523b1a1cf0465231
Yuriy Bondarenko <bondarenko-yri@yandex.ru>
2021-10-05 00:49:13 +0300

    start_card_room_and_problem_radio_btn

--- src/components/layout/layout_cards/layout_cards.js
+++ src/components/layout/layout_cards/layout_cards.js
@@ -1,8 +1,20 @@
 import './layout_cards.scss';
 
-import './components/cards/card_search_num/card_search_num.js';
-import './components/cards/card_account_reg/card_account_reg.js';
-import './components/cards/card_booking/card_booking.js';
-import './components/cards/calendar/calendar.js';
-import './components/cards/card_enter/card_enter.js';
-import './components/cards/card_room/card_room.js';
+import '../../button/button.js';
+
+import '../../cards/card_search_num/card_search_num.js';
+import '../../cards/card_booking/card_booking.js';
+import '../../cards/card_account_reg/card_account_reg.js';
+import '../../cards/calendar/calendar.js';
+import '../../cards/card_enter/card_enter.js';
+import '../../cards/card_room/card_room.js';
+
+import '../../mixins/masked_text_field/masked_text_field.js';
+import '../../mixins/date_dropdown/date_dropdown.js';
+import '../../mixins/text_field/text_field.js';
+import '../../mixins/dropdown/dropdown.js';
+import '../../mixins/button/button.js';
+import '../../mixins/btn_additional/btn_additional.js';
+import '../../mixins/radio_buttons/radio_buttons.js';
+import '../../mixins/toggle_btn/toggle_btn.js';
+import '../../mixins/rate_button/rate_button.js';
--- src/main.js
+++ src/main.js
@@ -6,40 +6,12 @@ import './scss/main.scss';
 
 // import './components/header/header.js';
 // import './components/footer/footer.js';
-import './components/button/button.js';
 
-// import './components/mixins/checkbox_buttons/checkbox_buttons.js';
-// import './components/mixins/checkbox_list/checkbox_list.js';
-// import './components/mixins/radio_buttons/radio_buttons.js';
-import './components/mixins/toggle_btn/toggle_btn.js';
-// import './components/mixins/like_button/like_button.js';
-// import './components/mixins/rate_button/rate_button.js';
-import './components/mixins/button/button.js';
-import './components/mixins/btn_additional/btn_additional.js';
-// import './components/mixins/link_main/link_main.js';
-// import './components/mixins/pagination/pagination.js';
-// import './components/mixins/range_slider/range_slider.js';
-// import './components/mixins/rich_chkbox_btn/rich_chkbox_btn.js';
-// import './components/mixins/information_block/information_block.js';
-
-// import './components/mixins/subscription_text_field/subscription_text_field.js';
-// import './components/mixins/filter_date_dropdown/filter_date_dropdown.js';
-import './components/mixins/date_dropdown/date_dropdown.js';
-import './components/mixins/text_field/text_field.js';
-import './components/mixins/dropdown/dropdown.js';
-// import './components/mixins/dropdown_facilities/dropdown_facilities.js';
-import './components/mixins/masked_text_field/masked_text_field.js';
-
-import './components/cards/card_search_num/card_search_num.js';
-import './components/cards/card_account_reg/card_account_reg.js';
-import './components/cards/card_booking/card_booking.js';
-import './components/cards/calendar/calendar.js';
-import './components/cards/card_enter/card_enter.js';
-import './components/cards/card_room/card_room.js';
-
-// import './components/layout/layout_form_element/layout';
+// import './components/layout/layout_form_element/layout_form_element';
+// import './components/layout/layout_form_element/layout_form_element.js';
 import './components/layout/layout_cards/layout_cards';
-// import './components/text_field/text_field.js';
+import './components/layout/layout_cards/layout_cards.js';
+
 
 
 // import './components/layout/layout_header_footer/layout';
