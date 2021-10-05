4ff94b55b0d564fc3022dcf94c3afecd82c38d52
Yuriy Bondarenko <bondarenko-yri@yandex.ru>
2021-10-06 00:25:54 +0300

    refactoring_page_header&footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,1 +1,5 @@
 import './footer.scss';
+
+import '../mixins/subscription_text_field/subscription_text_field.js';
+import '../mixins/nav_menu/nav_menu.js';
+import '../copyright/copyright.js';
---
+++ src/components/layout/layout_header_footer/layout_header_footer.js
@@ -0,0 +1,4 @@
+import './layout_header_footer.scss';
+
+import '../../header/header.js';
+import '../../footer/footer.js';
