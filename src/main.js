932585a689c6f35a5dd72228aa0dc5e3167ea39d
Yuriy Bondarenko <bondarenko-yri@yandex.ru>
2021-10-06 23:51:15 +0300

    room_card_slader_js_not_dots

--- src/main.js
+++ src/main.js
@@ -7,11 +7,11 @@ import './scss/main.scss';
 // import './components/layout/layout_form_element/layout_form_element';
 // import './components/layout/layout_form_element/layout_form_element.js';
 
-// import './components/layout/layout_cards/layout_cards';
-// import './components/layout/layout_cards/layout_cards.js';
+import './components/layout/layout_cards/layout_cards';
+import './components/layout/layout_cards/layout_cards.js';
 
-import './components/layout/layout_header_footer/layout_header_footer';
-import './components/layout/layout_header_footer/layout_header_footer.js';
+// import './components/layout/layout_header_footer/layout_header_footer';
+// import './components/layout/layout_header_footer/layout_header_footer.js';
 
 import json from './assets/json.json';
 
